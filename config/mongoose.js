bf780144c31d418064d0660e651980fabbdfcbc7
Ankush Pine <ankushpineofficial@gmail.com>
2023-08-05 22:22:59 +0530

    Updated

---
+++ config/mongoose.js
@@ -0,0 +1,23 @@
+
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+const DB = process.env.MONGO_DB_URL;
+
+// These set of line can be written in async await fashion, but I have followed the documentation. 
+mongoose.connect(DB).then(()=>{
+     console.log('connection successful');
+ }).catch((err) => console.log("no connection " + err));
+
+const db = mongoose.connection;
+
+// db.on('error', console.error.
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
+db.once('open',  function(){
+     console.log('Connected to Database :: MongoDB');
+});
+
+
+module.exports = db;
